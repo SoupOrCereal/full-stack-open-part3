830363ff915ebccd899170b59d377f5d20da3c8a
unknown <souporcerealemail@gmail.com>
2023-01-02 12:04:49 -0500

    3.4

--- index.js
+++ index.js
@@ -25,30 +25,46 @@ const persons = [
 ]
 
 app.get('/', (request, response) => {
+  //console.log('get>>/', request)
   response.send('<marquee scrollamount="32"><h1>Server Says :: "bee bop boop"</h1></marquee>')
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people<p><br />
-                        ${new Date().toDateString()} ${new Date().toLocaleTimeString()}`);
+  //console.log('get>>/info', request)
+  response.send(`<p>Phonebook has info for ${persons.length} people<p><br />
+                      ${new Date().toDateString()} ${new Date().toLocaleTimeString()}`);
 })
 
 app.get('/api/persons', (request, response) => {
+  //console.log('get>>/api/persons', request)
   response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+  //console.log('get>>/api/persons/:id', request)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  //console.log('delete>>/api/persons/:id', request)
+  const id = Number(request.params.id)
+  delPerson = persons.filter(person => person.id !== id)
+
+  delPerson?response.status(204).end():response.status(404).end()
+
+  // Functionality of delete added.
+  // ToDo :: apply deletion of person
+})
+
+// Start Server
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Started Server :: running on port ${PORT}`)
 })
