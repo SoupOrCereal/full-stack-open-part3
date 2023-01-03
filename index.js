86c01359a678addadced84b1ff02fb8913d2f71c
unknown <souporcerealemail@gmail.com>
2023-01-03 17:50:36 -0500

    3.8

--- index.js
+++ index.js
@@ -3,32 +3,66 @@ var morgan = require('morgan')
 
 const app = express()
 
-
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (reqq, ress) => JSON.stringify(reqq.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
+
+app.post('/api/persons', (request, response) => {
+  //console.log('post>>/api/persons', request)
+  const body = request?.body
+
+  if (!body) { 
+    return response.status(400).json({ 
+      error: 'no content provided' 
+    })
+  }else if (!body.name) { 
+    return response.status(400).json({ 
+      error: 'Name missing' 
+    })
+  }else if (!body.number) { 
+    return response.status(400).json({ 
+      error: 'Number missing' 
+    })
+  }else if (persons.find(person=>person.name===body.name)) { 
+    return response.status(400).json({ 
+      error: 'That name already exists in the phonebook' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.ceil(Math.random() * 1000000) // "Generate a new id for the phonebook entry with the Math.random"
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 app.get('/', (request, response) => {
   //console.log('get>>/', request)
@@ -66,39 +100,7 @@ app.delete('/api/persons/:id', (request, response) => {
   delPerson?response.status(204).end():response.status(404).end()
 })
  
-app.post('/api/persons', (request, response) => {
-  //console.log('post>>/api/persons', request)
-  console.log('persons POST received, body:', request.body)
-  const body = request?.body
 
-  if (!body) { 
-    return response.status(400).json({ 
-      error: 'no content provided' 
-    })
-  }else if (!body.name) { 
-    return response.status(400).json({ 
-      error: 'Name missing' 
-    })
-  }else if (!body.number) { 
-    return response.status(400).json({ 
-      error: 'Number missing' 
-    })
-  }else if (persons.find(person=>person.name===body.name)) { 
-    return response.status(400).json({ 
-      error: 'That name already exists in the phonebook' 
-    })
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number || false,
-    id: Math.ceil(Math.random() * 1000000) // "Generate a new id for the phonebook entry with the Math.random"
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
-})
 
 // Start Server
 const PORT = 3001
